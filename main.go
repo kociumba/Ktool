922f172f9d57f2fcad7d7703ffbc6b9c23397024
kociumba <kociumba1@gmail.com>
2023-07-26 22:21:42 +0200

    currency conversion function

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func modeSelect() {
 
 	prompt := &survey.Select{
 		Message: "app mode:",
-		Options: []string{"pricer", "sys info", "funny", "exit"},
+		Options: []string{"sys info", "pricer", "currency convert", "funny", "exit"},
 	}
 
 	survey.AskOne(prompt, &mode, survey.WithValidator(survey.Required))
@@ -44,6 +44,8 @@ func modeSelect() {
 		funny()
 	case mode == "sys info":
 		sysInfo()
+	case mode == "currency convert":
+		currencyConvert()
 	case mode == "exit":
 		cfmt.Errorln("exiting...")
 		return
@@ -56,6 +58,7 @@ func pricer() {
 	var discount = 0.0
 	var tax_version = ""
 	var tax = 0.0
+	var isItScam = ""
 
 	promptPrice := &survey.Input{
 		Message: "select the price:",
@@ -87,8 +90,13 @@ func pricer() {
 
 	result := math.Round(price*(1-(discount/100))*(1+tax)*100) / 100
 
-	cfmt.Successln(result)
-
+	fmt.Println("<----------DISCOUNTED PRICE---------->")
+	cfmt.Successln(" the discounted price is ", result)
+	if result > price {
+		isItScam = "the government is scamming you"
+		fmt.Println(ctc.ForegroundBright, isItScam, ctc.Reset)
+	}
+	fmt.Println("<------------------------------------>")
 }
 
 func funny() {
@@ -135,12 +143,66 @@ func sysInfo() {
 	c, _ := cpu.Info()
 
 	memoryInfo := fmt.Sprintf("Total: %v, Free:%v, UsedPercent:%f%%", v.Total, v.Free, v.UsedPercent)
-	osInfo := fmt.Sprintf("OS: %v, Uptime: %v", o.OS, o.Uptime)
-	cpuInfo := fmt.Sprintf("Vendor: %v, Model: %v", c[0].VendorID, c[0].ModelName)
+	osInfo := fmt.Sprintf("OS: %v, Uptime: %v, Procs: %v", o.OS, o.Uptime, o.Procs)
+	cpuInfo := fmt.Sprintf("Vendor: %v, Cores: %v, Mhz: %v, Model: %v", c[0].VendorID, c[0].Cores, c[0].Mhz, c[0].ModelName)
 
 	fmt.Println("<----------SYS INFO---------->")
 	fmt.Println(ctc.ForegroundYellow, "System:", osInfo, ctc.Reset)
 	fmt.Println(ctc.ForegroundBrightCyan, "Cpu:", cpuInfo, ctc.Reset)
 	fmt.Println(ctc.ForegroundBrightGreenBackgroundBlack, "Memory:", memoryInfo, ctc.Reset)
 	fmt.Println("<---------------------------->")
+}
+
+func currencyConvert() {
+
+	amount := 0.0
+	from := ""
+	to := ""
+
+	promptAmount := &survey.Input{
+		Message: "select the amount:",
+		Help:    "amaout of money to convert, currency nonspecific",
+	}
+
+	survey.AskOne(promptAmount, &amount, survey.WithValidator(survey.Required))
+
+	promptFrom := &survey.Input{
+		Message: "convert from:",
+		Help:    "use an international currency code like 'USD', if it doesn't work it means the api i'm using doesn't support this currency",
+	}
+
+	survey.AskOne(promptFrom, &from, survey.WithValidator(survey.Required))
+
+	promptTo := &survey.Input{
+		Message: "convert to:",
+		Help:    "use an international currency code like 'USD', if it doesn't work it means the api i'm using doesn't support this currency",
+	}
+
+	survey.AskOne(promptTo, &to, survey.WithValidator(survey.Required))
+
+	client := http.Client{}
+
+	url := fmt.Sprintf("https://api.exchangerate.host/convert?from=%v&to=%v", from, to)
+
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var result map[string]interface{}
+	json.NewDecoder(resp.Body).Decode(&result)
+
+	rate := result["info"].(map[string]interface{})["rate"].(float64)
+	fmt.Println("<----------CONVERSION---------->")
+	fmt.Println(ctc.ForegroundBright, "current exchange from", from, "to", to, "is", rate, ctc.Reset)
+
+	exchangedAmount := math.Round((amount*rate)*100) / 100
+
+	fmt.Println(ctc.ForegroundBrightGreen, amount, from, "is", exchangedAmount, to, ctc.Reset)
+	fmt.Println("<------------------------------>")
 }
