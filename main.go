9a85fdd4be782d53c55ab1562386f2cb7863b014
kociumba <kociumba1@gmail.com>
2023-07-26 08:35:48 +0200

    Update main.go

--- main.go
+++ main.go
@@ -18,25 +18,6 @@ func main() {
 		fmt.Println(mode)
 	}
 
-	// if err := keyboard.Open(); err != nil {
-	// 	panic(err)
-	// }
-	// defer func() {
-	// 	_ = keyboard.Close()
-	// }()
-
-	// fmt.Println("Press ESC to quit")
-	// for {
-	// 	char, key, err := keyboard.GetKey()
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	fmt.Printf("end: rune %q, key %X\r\n", char, key)
-	// 	if key == keyboard.KeyEsc {
-	// 		break
-	// 	}
-	// }
-
 }
 
 func pricer() {
