aa05cad81fa0545e03b715ccf966eeabdd12165e
kociumba <79542688+kociumba@users.noreply.github.com>
2024-03-28 19:42:34 +0100

    fixed notes and made them completely cross platform 😎

--- notes.go
+++ notes.go
@@ -4,17 +4,25 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"log"
 	"os"
-	"path/filepath"
+	"os/user"
 	"regexp"
-	"runtime"
 
 	"github.com/AlecAivazis/survey/v2"
-	"github.com/wzshiming/ctc"
 )
 
 // note actions handler
 func notes() {
+	u, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	homeDir := fmt.Sprintf(u.HomeDir + `/ktool/notes.txt`)
+	err = os.MkdirAll(u.HomeDir+"/ktool", 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	noteAction := ""
 
@@ -30,17 +38,17 @@ func notes() {
 
 	switch {
 	case noteAction == "add a note":
-		addNote()
+		addNote(homeDir)
 	case noteAction == "read notes":
-		readNotes()
+		readNotes(homeDir)
 	case noteAction == "delete notes":
-		deleteNotes()
+		deleteNotes(homeDir)
 	}
 
 }
 
 // add notes and write to disk
-func addNote() {
+func addNote(homeDir string) {
 	var newNote string
 
 	promptNote := &survey.Input{
@@ -52,26 +60,26 @@ func addNote() {
 
 	survey.AskOne(promptNote, &newNote, survey.WithValidator(survey.Required))
 
-	targetFile := "notes.txt"
-	absPath := targetFile
+	// targetFile := "notes.txt"
+	// absPath := targetFile
 
-	if runtime.GOOS == "windows" {
-		root := "/Program Files (x86)/fuck you inc/ktool"
-		absPath = filepath.Join(root, targetFile)
-	}
+	// if runtime.GOOS == "windows" {
+	// 	root := "/Program Files (x86)/fuck you inc/ktool"
+	// 	absPath = filepath.Join(root, targetFile)
+	// }
 
-	_, err := os.Stat(absPath)
+	_, err := os.Stat(homeDir)
 	if os.IsNotExist(err) {
 		// File not found, create it
-		_, err = os.Create(absPath)
+		_, err = os.Create(homeDir)
 		if err != nil {
 			fmt.Println("Error creating file:", err)
 			return
 		}
-		fmt.Println(ctc.ForegroundYellow, "The note save file has been created. This should only happen once if you are on windows", ctc.Reset)
+		// fmt.Println(ctc.ForegroundYellow, "The note save file has been created. This should only happen once if you are on windows", ctc.Reset)
 	}
 
-	f, err := os.OpenFile(absPath, os.O_APPEND|os.O_WRONLY, 0600)
+	f, err := os.OpenFile(homeDir, os.O_APPEND|os.O_WRONLY, 0600)
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		return
@@ -90,148 +98,164 @@ func addNote() {
 }
 
 // read notes from disk
-func readNotes() {
+func readNotes(homedir string) {
 
-	if runtime.GOOS == "windows" {
-		f, err := os.Open("/Program Files (x86)/fuck you inc/ktool/notes.txt")
-		if err != nil {
-			fmt.Println("Error opening file:", err)
-			return
-		}
+	// if runtime.GOOS == "windows" {
+	// 	f, err := os.Open("/Program Files (x86)/fuck you inc/ktool/notes.txt")
+	// 	if err != nil {
+	// 		fmt.Println("Error opening file:", err)
+	// 		return
+	// 	}
 
-		defer f.Close()
+	// 	defer f.Close()
 
-		scanner := bufio.NewScanner(f)
-		counter := 1
+	// 	scanner := bufio.NewScanner(f)
+	// 	counter := 1
 
-		for scanner.Scan() {
-			fmt.Println(counter, scanner.Text())
-			counter++
-		}
-	} else {
-		f, err := os.Open("notes.txt")
+	// 	for scanner.Scan() {
+	// 		fmt.Println(counter, scanner.Text())
+	// 		counter++
+	// 	}
+	// } else {
+	// 	f, err := os.Open("notes.txt")
 
-		if err != nil {
-			fmt.Println("Error opening file:", err)
-			return
-		}
+	// 	if err != nil {
+	// 		fmt.Println("Error opening file:", err)
+	// 		return
+	// 	}
 
-		defer f.Close()
+	// 	defer f.Close()
 
-		scanner := bufio.NewScanner(f)
-		counter := 1
+	// 	scanner := bufio.NewScanner(f)
+	// 	counter := 1
 
-		for scanner.Scan() {
-			fmt.Println(counter, scanner.Text())
-			counter++
-		}
+	// 	for scanner.Scan() {
+	// 		fmt.Println(counter, scanner.Text())
+	// 		counter++
+	// 	}
+	// }
+
+	f, err := os.Open(homedir)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	counter := 1
+
+	for scanner.Scan() {
+		fmt.Println(counter, scanner.Text())
+		counter++
 	}
 
 	// notes()
 }
 
 // handler for deleting notes
-func deleteNotes() {
-	if runtime.GOOS == "windows" {
-		f, err := os.Open("/Program Files (x86)/fuck you inc/ktool/notes.txt")
-		if err != nil {
-			fmt.Println("Error opening file:", err)
-			return
-		}
-
-		defer f.Close()
-
-		counter := 1
-
-		notes := []string{}
-		scanner := bufio.NewScanner(f)
-		for scanner.Scan() {
-			notes = append(notes, scanner.Text())
-			counter++
-		}
-
-		notesToDelete := []string{}
-
-		prompt := &survey.MultiSelect{
-			Renderer: survey.Renderer{},
-			Message:  "choose which notes to delete",
-			Options:  notes,
-			Default:  nil,
-			Help:     "changes made here are permanent",
-		}
-
-		survey.AskOne(prompt, &notesToDelete, survey.WithValidator(survey.Required))
-
-		fmt.Println(notesToDelete)
-
-		err = f.Close()
-		if err != nil {
-			fmt.Println("Error closing file:", err)
-			return
-		}
-
-		deleter(notesToDelete)
+func deleteNotes(homedir string) {
+	// if runtime.GOOS == "windows" {
+	f, err := os.Open(homedir)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
 	}
+
+	defer f.Close()
+
+	counter := 1
+
+	notes := []string{}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		notes = append(notes, scanner.Text())
+		counter++
+	}
+
+	notesToDelete := []string{}
+
+	prompt := &survey.MultiSelect{
+		Renderer: survey.Renderer{},
+		Message:  "choose which notes to delete",
+		Options:  notes,
+		Default:  nil,
+		Help:     "changes made here are permanent",
+	}
+
+	survey.AskOne(prompt, &notesToDelete, survey.WithValidator(survey.Required))
+
+	fmt.Println(notesToDelete)
+
+	err = f.Close()
+	if err != nil {
+		fmt.Println("Error closing file:", err)
+		return
+	}
+
+	deleter(notesToDelete, homedir)
+	// }
 
 	// notes()
 }
 
 // deletes notes from disk
-func deleter(notesToDelete []string) error {
-	if runtime.GOOS == "windows" {
+func deleter(notesToDelete []string, homedir string) error {
+	// if runtime.GOOS == "windows" {
 
-		file, err := os.Open("/Program Files (x86)/fuck you inc/ktool/notes.txt")
-		if err != nil {
-			return fmt.Errorf("error opening file: %v", err)
-		}
-
-		tmpFile, err := os.CreateTemp("", "notes.txt")
-		if err != nil {
-			return fmt.Errorf("error creating temporary file: %v", err)
-		}
-
-		defer os.Remove(tmpFile.Name())
-
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			line := scanner.Text()
-			isMatched := false
-			for _, note := range notesToDelete {
-				matched, _ := regexp.MatchString("^"+note+"$", line)
-				if matched {
-					isMatched = true
-					break
-				}
-			}
-			if !isMatched {
-				_, err := io.WriteString(tmpFile, line+"\n")
-				if err != nil {
-					fmt.Printf("error writing to temporary file: %v", err)
-				}
-			}
-		}
-
-		err = file.Close()
-		if err != nil {
-			return fmt.Errorf("error closing file: %w", err)
-		}
-
-		err = tmpFile.Close()
-		if err != nil {
-			return fmt.Errorf("error closing temporary file: %w", err)
-		}
-
-		err = os.Remove("/Program Files (x86)/fuck you inc/ktool/notes.txt")
-		if err != nil {
-			fmt.Printf("error removing original file: %v", err)
-		}
-
-		err = os.Rename(tmpFile.Name(), "/Program Files (x86)/fuck you inc/ktool/notes.txt")
-		if err != nil {
-			fmt.Printf("error renaming temporary file: %v", err)
-		}
-
+	file, err := os.Open(homedir)
+	if err != nil {
+		return fmt.Errorf("error opening file: %v", err)
 	}
+
+	tmpFile, err := os.CreateTemp("", "notes.txt")
+	if err != nil {
+		return fmt.Errorf("error creating temporary file: %v", err)
+	}
+
+	defer os.Remove(tmpFile.Name())
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		isMatched := false
+		for _, note := range notesToDelete {
+			matched, _ := regexp.MatchString("^"+note+"$", line)
+			if matched {
+				isMatched = true
+				break
+			}
+		}
+		if !isMatched {
+			_, err := io.WriteString(tmpFile, line+"\n")
+			if err != nil {
+				fmt.Printf("error writing to temporary file: %v", err)
+			}
+		}
+	}
+
+	err = file.Close()
+	if err != nil {
+		return fmt.Errorf("error closing file: %w", err)
+	}
+
+	err = tmpFile.Close()
+	if err != nil {
+		return fmt.Errorf("error closing temporary file: %w", err)
+	}
+
+	err = os.Remove(homedir)
+	if err != nil {
+		fmt.Printf("error removing original file: %v", err)
+	}
+
+	err = os.Rename(tmpFile.Name(), homedir)
+	if err != nil {
+		fmt.Printf("error renaming temporary file: %v", err)
+	}
+
+	// }
 
 	return nil
 }
